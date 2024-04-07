b84d525068c759f149d598cfa05544857879e422
Daniel-Burbridge-Developer <Daniel.Burbridge.Developer@gmail.com>
2024-04-07 18:31:37 +0800

    tried on my own, getting boots to write pseudocode now

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/*", http.FileServer(http.Dir(".")))
+
 	corsMux := middlewareCors(mux)
+	readiMux := middlewareReadiness(corsMux)
 
-	httpServer := http.Server{Addr: "localhost:8080", Handler: corsMux}
+	httpServer := &http.Server{Addr: "localhost:8080", Handler: readiMux}
 	httpServer.ListenAndServe()
-
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -20,6 +22,15 @@ func middlewareCors(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareReadiness(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 		next.ServeHTTP(w, r)
 	})
 }
